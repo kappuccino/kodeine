1c1b28ee4dc996513be91c8f3c1c1d5bfe8bae09
Benjamin <bm@kappuccino.org>
2012-11-14 14:26:15 +0100

    Ajout des privilèges sur les sous-menu
    Sur les modules Business, Config, Content et User, les paramètres du profil permettent d'activer ou de désactiver certaines options.

--- app/module/business/ui/menu.php
+++ app/module/business/ui/menu.php
@@ -1,55 +1,46 @@
 
-<div id="sub_nav" class="text <?php #$tmp = $app->filterGet('admin'); echo ($tmp['adminSubMenu'] == '') ? 'icon text' : $tmp['adminSubMenu']; unset($tmp) ?>"><div class="wrapper clearfix">
+<div id="sub_nav" class="text"><div class="wrapper clearfix">
 
 	<ul class="left">
 		<li class="<?php echo isMe('/business/$') ? 'me':'' ?>">
-			<a href="/admin/business/">
-				<!-- <img src="/admin/business/ui/img/business.png" /> -->
-				<span>Commande</span>
-			</a>
+			<a href="/admin/business/"><span>Commande</span></a>
 		</li>
+
 		<li class="<?php echo isMe('/business/carriage') ? 'me':'' ?>">
-			<a href="/admin/business/carriage">
-				<!-- <img src="/admin/business/ui/img/carriage.png" /> -->
-				<span>Frais de port</span>
-			</a>
+			<a href="/admin/business/carriage"><span>Frais de port</span></a>
 		</li>
+
+		<?php if($app->userCan('business.coupon')){ ?>
 		<li class="<?php echo isMe('/business/coupon') ? 'me':'' ?>">
-			<a href="/admin/business/coupon">
-				<!-- <img src="/admin/business/ui/img/coupon.png" /> -->
-				<span>Coupon</span>
-			</a>
+			<a href="/admin/business/coupon"><span>Coupon</span></a>
 		</li>
+		<?php } ?>
+
 		<li class="<?php echo isMe('/business/hist') ? 'me':'' ?>">
-			<a href="/admin/business/hist">
-				<!-- <img src="/admin/business/ui/img/hist.png" /> -->
-				<span>Historique</span>
-			</a>
+			<a href="/admin/business/hist"><span>Historique</span></a>
 		</li>
+
 		<li class="<?php echo isMe('/business/shop') ? 'me':'' ?>">
-			<a href="/admin/business/shop">
-				<!-- <img src="/admin/shop/ui/shop.png" /> -->
-				<span>Shop</span>
-			</a>
+			<a href="/admin/business/shop"><span>Shop</span></a>
 		</li>
+
+		<?php if($app->userCan('business.account')){ ?>
 		<li class="<?php echo isMe('/business/account') ? 'me':'' ?>">
-			<a href="/admin/business/account">
-				<!-- <img src="/admin/business/ui/img/account.png" /> -->
-				<span>Comptes</span>
-			</a>
+			<a href="/admin/business/account"><span>Comptes</span></a>
 		</li>
+		<?php } ?>
+
+		<?php if($app->userCan('business.tva')){ ?>
 		<li class="<?php echo isMe('/business/tax') ? 'me':'' ?>">
-			<a href="/admin/business/tax">
-				<!-- <img src="/admin/business/ui/img/tax.png" /> -->
-				<span>TVA</span>
-			</a>
+			<a href="/admin/business/tax"><span>TVA</span></a>
 		</li>
+		<?php } ?>
+
+		<?php if($app->userCan('business.config')){ ?>
 		<li class="<?php echo isMe('/business/config') ? 'me':'' ?>">
-			<a href="/admin/business/config">
-				<!-- <img src="/admin/business/ui/img/config.png" /> -->
-				<span>Config</span>
-			</a>
+			<a href="/admin/business/config"><span>Config</span></a>
 		</li>
+		<?php } ?>
 	</ul>
 	
 	<ul class="right"></ul>
