6bc538e46a222b57a2c7a627f0005118f2584cf7
Paul Sore <paul.sore@gmail.com>
2012-11-22 16:55:27 +0100

    Update app/module/business/api.business.php
    test sync

--- app/module/business/api.business.php
+++ app/module/business/api.business.php
@@ -1,6 +1,6 @@
 <?php
 /* + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - 
-	Last release		2010.10.18
+	Last release		2010.10.18-b
 + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - + - */
 
 class business extends coreApp {
