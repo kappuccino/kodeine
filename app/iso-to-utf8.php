32ddeb4026cfc385045b5ff21649834422ce8331
Benjamin Mosnier <bm@kappuccino.org>
2012-12-13 17:18:11 +0100

    UTF8 (patch auto)
    Signed-off-by: Benjamin Mosnier <bm@kappuccino.org>

--- app/iso-to-utf8.php
+++ app/iso-to-utf8.php
@@ -3,9 +3,11 @@
 	require(__DIR__.'/module/core/helper/app.php');
 	$app = new coreApp();
 
-	$cmd = 'sh '.KROOT.'/app/iso-to-utf8-linux-php '.KROOT.'/user/test benjamin benjamin';
+
+	$cmd = 'sh '.KROOT.'/app/iso-to-utf8-linux-php '.KROOT.'/user/test '.posix_getuid().' '.posix_getgid();
 	echo $cmd."\n\n\n";
 #	die();
+
 	var_export(
 	#	exec($cmd, $out, $ret)
 		$app->helperPipeExec($cmd)
