1db87818a99ab4093a76fc8eeae258b27d47fa51
Benjamin Mosnier <bm@kappuccino.org>
2012-12-13 17:38:15 +0100

    UTF8 (php wrapper)
    Signed-off-by: Benjamin Mosnier <bm@kappuccino.org>

--- app/iso-to-utf8.php
+++ app/iso-to-utf8.php
@@ -3,8 +3,9 @@
 	require(__DIR__.'/module/core/helper/app.php');
 	$app = new coreApp();
 
+	$exe = isset($_GET['do']) ? 'OK' : 'XX';
 
-	$cmd = 'sh '.KROOT.'/app/iso-to-utf8-linux-php '.KROOT.'/user/test '.posix_getuid().' '.posix_getgid();
+	$cmd = 'sh '.KROOT.'/app/iso-to-utf8-linux-php '.KROOT.'/user/test '.posix_getuid().' '.posix_getgid().' '.$exe;
 	echo $cmd."\n\n\n";
 #	die();
 
