a632423dcfa5883e8cbc645b05391597470de1b8
Benjamin <bm@kappuccino.org>
2013-01-08 16:26:33 +0100

    CSS
    Compilation des LESS
    
    Signed-off-by: Benjamin <bm@kappuccino.org>

--- module/core/includes/head.php
+++ module/core/includes/head.php
@@ -1,8 +1,9 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 
+	<link rel="stylesheet" type="text/css" href="/admin/core/ui/css/_style.css" />
 	<?php
-		echo $app->less('/admin/core/ui/css/_style.less');
-		include(__DIR__.'/less.php');
+		#echo $app->less('/admin/core/ui/css/_style.less');
+	#	include(__DIR__.'/less.php');
 	?>
 
 	<link href='http://fonts.googleapis.com/css?family=Droid+Sans:400,700' rel='stylesheet' type='text/css'>
