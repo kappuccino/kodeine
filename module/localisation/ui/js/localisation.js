2f3188d2ab22d49ab85ca279deabd98ced89fc6a
rboyer <rb@kappuccino.org>
2014-04-25 10:12:44 +0200

    TinyMCE resizable dans gestion traduction des labels

--- module/localisation/ui/js/localisation.js
+++ module/localisation/ui/js/localisation.js
@@ -88,7 +88,7 @@ function on(iso){
 			theme_advanced_toolbar_location		: "top",
 			theme_advanced_toolbar_align		: "left",
 			theme_advanced_statusbar_location	: "bottom",
-			theme_advanced_resizing				: false,
+			theme_advanced_resizing				: true
 		});
 	}
 }
