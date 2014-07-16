f5c43cb74c8d6696a414b8ab1979fbe3f09d0dd7
Benjamin Mosnier <bm@kappuccino.org>
2014-07-16 12:00:36 +0200

    Mod Business (add to cart)
    Possibilité d'ajouter un id_group lors de la phase d'ajout d'un produit au panier qui n'est pas le group courant
    
    Signed-off-by: Benjamin Mosnier <bm@kappuccino.org>

--- module/business/api.business.php
+++ module/business/api.business.php
@@ -357,13 +357,19 @@ public function businessCartAdd($opt=array()){
 	$quantity	= isset($opt['quantity']) ? $opt['quantity'] : 1;
 
 	# Verifier que le stock permet d'ajouter cette quantity
-	$c = $this->apiLoad('content')->contentGet(array(
-		'id_content'	=> $id_content,
-		'debug'			=> false
-	));
+	$get = array(
+		'id_content' => $id_content,
+		'useChapter' => false,
+		'debug'		 => 0
+	);
+
+	// Forcer le group si on le demande
+	if($opt['id_group']) $get['id_group'] = $opt['id_group'];
+
+	$c = $this->apiLoad('content')->contentGet($get);
 
 	if($c['contentStockNeg']){
-		$quantity = $quantity;
+	//	$quantity = $quantity;
 	}else
 	if($c['contentStock'] - $quantity < 0){
 		$quantity = $c['contentStock'];
@@ -399,7 +405,7 @@ public function businessCartAdd($opt=array()){
 	
 
 	# Mettre a jour le STOCK pour le CONTENT
-	$this->dbQuery("UPDATE k_content SET contentStock = contentStock - ".$quantity." WHERE id_content=".$id_content);
+	$this->dbQuery("UPDATE k_content SET contentStock = contentStock - (".$quantity.") WHERE id_content=".$id_content);
 	if($opt['debug']) $this->pre($this->db_query, $this->db_error);
 
 	# Update CART
